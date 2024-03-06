813bb33690cf200f209f323fedc4a22c6b4a01f3
W. Shashindu Chamika <126416494+WShashinduChamika@users.noreply.github.com>
2024-03-06 12:58:51 +0530

    Set up connection between firebase project and mobile application #15

--- aquo/lib/main.dart
+++ aquo/lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:aquo/firebase_options.dart';
 import 'package:aquo/screens/home.dart';
 import 'package:aquo/screens/splash_screen.dart';
 import 'package:aquo/screens/start_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
